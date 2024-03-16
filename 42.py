717b3b57e06a526785172ff8c3bb30e75b90b824
cosmo 1 <ismoilovjavohir02@gmail.com>
2024-03-16 21:04:08 +0500

    flask

--- 42.py
+++ 42.py
@@ -1,2 +1,20 @@
-print('salom')
-print('no')
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("add.html")
+
+
+@app.route("/add")
+def adder():
+    a = request.args.get("a", "")
+    b = request.args.get("b", "")
+    a = int(a)
+    b = int(b)
+
+    return f"sum : {a+b}"   
+if __name__ == "__main__":
+    app.run(port=4200, debug= True)
+
