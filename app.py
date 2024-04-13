a1fac1b0fe689a8f9d22ace87d3c9d2d4701af67
cosmo 1 <ismoilovjavohir02@gmail.com>
2024-04-13 21:53:04 +0500

    find bug

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from Flask import Flask , render_template, request, session, make_response
+from flask import Flask , render_template, request, session, make_response
 from articles import Article
 import hashlib
 
